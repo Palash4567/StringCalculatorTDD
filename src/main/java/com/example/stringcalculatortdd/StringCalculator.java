91291e68aa54bbbfe77b13198a4d7101c6106040
Palash4567 <palash.khobragade@humancloud.co.in>
2025-01-17 17:28:17 +0530

    Working code for adding two numbers

--- src/main/java/com/example/stringcalculatortdd/StringCalculator.java
+++ src/main/java/com/example/stringcalculatortdd/StringCalculator.java
@@ -1,10 +1,23 @@
 package com.example.stringcalculatortdd;
 
+import static org.springframework.expression.common.ExpressionUtils.toInt;
+
 public class StringCalculator {
     public static int add(String input) {
-        if(input.isEmpty()) {
+        if (input.isEmpty()) {
             return 0;
         }
-        return Integer.parseInt(input) ;
+
+        //code for adding two numbers
+        String[] numbers = input.split(",");
+        int sum = 0;
+        if (numbers.length > 1) {
+            sum = toInt(numbers[0]) + toInt(numbers[1]);
+        }
+        return sum;
+    }
+
+    public static int toInt(String number) {
+        return Integer.parseInt(number);
     }
 }
