9afe7dcb40661b0e22ab6411c18e7564eef5df4c
Palash4567 <palash.khobragade@humancloud.co.in>
2025-01-17 17:23:39 +0530

    Working code for adding one Number

--- src/main/java/com/example/stringcalculatortdd/StringCalculator.java
+++ src/main/java/com/example/stringcalculatortdd/StringCalculator.java
@@ -5,6 +5,6 @@ public class StringCalculator {
         if(input.isEmpty()) {
             return 0;
         }
-        return -1;   // Taking -1 just to pass the empty String test case.
+        return Integer.parseInt(input) ;
     }
 }
