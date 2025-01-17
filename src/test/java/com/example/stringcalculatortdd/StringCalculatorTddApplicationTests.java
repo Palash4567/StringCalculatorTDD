a405a63f1824738be2e4cfa1cb7d153579f911e1
Palash4567 <palash.khobragade@humancloud.co.in>
2025-01-17 17:13:59 +0530

    Initial commit

---
+++ src/test/java/com/example/stringcalculatortdd/StringCalculatorTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stringcalculatortdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringCalculatorTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
