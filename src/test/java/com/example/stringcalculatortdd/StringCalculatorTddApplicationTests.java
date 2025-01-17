0914f1290364cbdb381f7222adf4dcaaae7097e7
Palash4567 <palash.khobragade@humancloud.co.in>
2025-01-17 17:17:43 +0530

    Failed Test for empty String

--- src/test/java/com/example/stringcalculatortdd/StringCalculatorTddApplicationTests.java
+++ src/test/java/com/example/stringcalculatortdd/StringCalculatorTddApplicationTests.java
@@ -3,11 +3,18 @@ package com.example.stringcalculatortdd;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class StringCalculatorTddApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testEmptyString(){
+        assertEquals(0, StringCalculator.add(""));
     }
 
 }
