85e0cfce0787eee2c4f107a451407a6721403529
Palash4567 <palash.khobragade@humancloud.co.in>
2025-01-17 18:16:44 +0530

    Refactored Code

--- src/main/java/com/example/stringcalculatortdd/StringCalculator.java
+++ src/main/java/com/example/stringcalculatortdd/StringCalculator.java
@@ -1,32 +1,35 @@
 package com.example.stringcalculatortdd;
 
-import static org.springframework.expression.common.ExpressionUtils.toInt;
-
 public class StringCalculator {
+
     public static int add(String input) {
         if (input.isEmpty()) {
             return 0;
         }
 
-        //code for adding other delimeters
+        // Code for adding other delimiters
         String delimiter = ",|\n";
         if (input.matches("//(.*)\n(.*)")) {
             delimiter = Character.toString(input.charAt(2));
             input = input.substring(4);
         }
         String[] numbers = input.split(delimiter);
+        return calculateSum(numbers);
+    }
+
+    private static int calculateSum(String[] numbers) {
         int sum = 0;
         for (String number : numbers) {
             int num = toInt(number);
             if (num < 0) {
                 throw new IllegalArgumentException("Negatives not allowed: " + num);
             }
-            sum = sum + num;
+            sum += num;
         }
         return sum;
     }
 
-    public static int toInt(String number) {
+    private static int toInt(String number) {
         return Integer.parseInt(number);
     }
 }
